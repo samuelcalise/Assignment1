d107d87fa1d481f794acd6c868c4823312e39a0b
samuelcalise <113198869+samuelcalise@users.noreply.github.com>
2023-09-17 20:50:55 +0000

    Finally working.... but at what cost :,)

---
+++ fgroups/src/main.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let mut person_databank: HashMap<String, Vec<String>> = HashMap::new();
+
+    for line in io::stdin().lock().lines()
+     {
+        let l_input_string = line.expect("Failed to read line!");
+
+        let mut l_string_parts = l_input_string.trim().split_whitespace();
+
+        if let Some(fingerprint_string) = l_string_parts.next() 
+        {
+            let l_fingerprint = fingerprint_string.to_string();
+            
+            let mut l_person_name = String::new();
+
+            if let Some(name_string) = l_string_parts.next() 
+            {
+                l_person_name = name_string.to_string();
+            }
+            
+            //Insert Exception Handling
+            if l_fingerprint.len() >= 512
+            {
+                continue;
+            }
+            else
+            {
+                let l_user_input = person_databank.entry(l_fingerprint.clone()).or_insert(vec![]);
+                l_user_input.push(l_person_name.clone());
+            }
+        }
+    }
+
+    let l_num_totalfingers = person_databank.len();
+
+    let mut l_counter_blanklines = 0;
+    for (_element_fingerprint, element_personname) in &person_databank
+    {
+       for iter_personname in element_personname
+       {
+            println!("{}", iter_personname);
+       }
+       if l_counter_blanklines < l_num_totalfingers - 1
+       {
+            l_counter_blanklines += 1;
+            println!("");
+       }
+
+    }
+}
