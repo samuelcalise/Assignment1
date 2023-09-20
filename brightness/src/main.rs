2e34fd448383dd908472099f881503e6406f7ddf
Claudia Deverdits <88414158+claudiadeverdits@users.noreply.github.com>
2023-09-20 18:57:24 +0000

    changes to brightness

--- brightness/src/main.rs
+++ brightness/src/main.rs
@@ -1,9 +1,31 @@
 use csc411_image::{Read, GrayImage};
 use std::env;
+use std::io;
+use std::io::BufRead;
 
 fn main() {
-    let input = env::args().nth(1);
-    assert!(env::args().len() <= 2);
+    //let mut input = env::args().nth(1);
+    let args: Vec<String> = env::args().collect();
+    // let string: String = String::new();
+    // string = input;
+
+    // match input {
+    //     Ok(value) => {
+    //         assert!(env::args().len() <= 2);
+    //     }
+    //     Err(error) => {
+
+    //     }
+    // }
+    match args.len() {
+        1 => { //stanard input
+            let mut input = String::new();
+            io::stdin().read_line(& mut input);
+            let img = GrayImage::read(&str input);
+        }
+    }
+
+    
 
     let img = GrayImage::read(input.as_deref()).unwrap();
 
